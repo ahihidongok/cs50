967084868a0bccc0c2a074e379841c0d46505b56
ahihidongok <ahihidongok@users.noreply.github.com>
2019-09-23 23:55:29 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -13,18 +13,18 @@ int main(void)
     int space;
     int hash_r;
     int hash_l;
-    
-   do
+
+    do
     {
-       printf("\nHeight: ");
-       scanf("%d",&height);
-       //printf("..%d",height);
+        printf("\nHeight: ");
+        scanf("%d", &height);
+        //printf("..%d",height);
     }
     while ((height < 1) || (height > 9));
     
     for (rows = 1; rows <= height; rows++)
     {
-        for (space = (height-rows); space >0; space--)
+        for (space = (height - rows); space > 0; space--)
         {
             printf(" ");
         }
